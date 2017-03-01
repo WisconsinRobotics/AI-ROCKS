e00a043b10a98304bee5c56865c3aa3d5349a394
TJ Bindseil <bindseil@wisc.edu>
2017-02-28 20:42:06 -0600

    added logic to check first and last gaps. removed final pieces of psuedocode with respect to compass stuff

--- AI-ROCKS/Drive/DriveStates/GPSDriveState.cs
+++ AI-ROCKS/Drive/DriveStates/GPSDriveState.cs
@@ -3,6 +3,9 @@
 using ObstacleLibrarySharp;
 using LRFLibrarySharp;
 using System.Collections.Generic;
+using System.Linq;
+
+using AI_ROCKS.Drive.Utils;
 
 namespace AI_ROCKS.Drive.DriveStates
 {
@@ -10,23 +13,19 @@ namespace AI_ROCKS.Drive.DriveStates
     class GPSDriveState : IDriveState
     {
         private const float DIRECTION_VATIANCE_NOISE = .001f; // gives threshold that "straight" is considered
+        private const long LRF_MAX_RELIABLE_DISTANCE = 6000;    // TODO get from LRFLibrary
 
         private double idealDirection;
         GPS finalGPS;
 
-        PacketHandlers.GPSHandler gpsHandler;
         GPS currGPS;
-        //PacketHandlers.CompassHandler compassHandler;  NOT IMPLEMENTED YET
-        // Compass currCompass;
+        short currCompass;
 
         DriveCommand command;
 
         public GPSDriveState()
         {
-            this.gpsHandler = new PacketHandlers.GPSHandler();
-            // this.compassHandler = new PacketHandlers.compassHandler();
         }
-
 
         /// <summary>
         /// Find the next DriveCommand to be issued to ROCKS.
@@ -34,13 +33,18 @@ namespace AI_ROCKS.Drive.DriveStates
         /// <returns>DriveCommand - the next drive command for ROCKS to execute.</returns>
         public DriveCommand FindNextDriveCommand()
         {
-            // Get "current" data from AscentShimLayer in form of GPS object
-            currGPS = gpsHandler.Data;
-            currCompass = compassHandler.Data;
+            currGPS = AI_ROCKS.PacketHandlers.AscentPacketHandler.GPSData;
+            currCompass = AI_ROCKS.PacketHandlers.AscentPacketHandler.Compass;
 
-            // Do GPS driving
-            // find angle between our gps and the end gps with trig 
-            idealDirection = Math.Atan2((finalGPS.Latitude - currGPS.Latitude), (finalGPS.Longitude - finalGPS.Longitude));
+            // get data in good form 
+            float finalLat, finalLong, currLat, currLong;
+            finalLat = finalGPS.LatDegrees + (finalGPS.LatMinutes / 60f) + (finalGPS.LatSeconds / 60f / 60f);
+            finalLong = finalGPS.LongDegrees+ (finalGPS.LongMinutes / 60f) + (finalGPS.LongSeconds / 60f / 60f);
+            currLat = currGPS.LatDegrees + (currGPS.LatMinutes / 60f) + (currGPS.LatSeconds / 60f / 60f);
+            currLong = currGPS.LongDegrees + (currGPS.LongMinutes / 60f) + (currGPS.LongSeconds / 60f / 60f);
+            
+            // calculate ideal direction
+            idealDirection = Math.Atan2((finalLat - currLat), (finalLong - currLong));
             idealDirection = idealDirection * (180 / Math.PI);
             idealDirection = -idealDirection;
             idealDirection = idealDirection + 90;
@@ -55,6 +59,9 @@ namespace AI_ROCKS.Drive.DriveStates
             }
 
             // not aligned with endGPS point, need to turn
+            // the math here is strange due to compass vs unit circle stuff
+            // the first case takes care of all time when the ideal direction is in some way east of us,
+            // and the second case takes care of all time when the ideal direction is in some way west of us
             double opposite = (idealDirection + 180) % 360;
             if (idealDirection < opposite) // this means that modulo was not necessary ie ideal direction < 180
             {
@@ -116,9 +123,82 @@ namespace AI_ROCKS.Drive.DriveStates
             double bestAngle = Double.MaxValue;
             double angle;
 
-            // initial and final gaps
+            // Check first and last Region gaps (may be same Region if only one Region)
+            Region firstRegion = regions.ElementAt(0);
+            Region lastRegion = regions.ElementAt(regions.Count - 1);
 
+            // left and right fields of view (FOV)
+            Line lrfLeftFOV =
+                new Line(new Coordinate(0, 0, CoordSystem.Cartesian), new Coordinate(-1 * LRF_MAX_RELIABLE_DISTANCE, 0, CoordSystem.Cartesian));
+            Line lrfRightFOV =
+                new Line(new Coordinate(0, 0, CoordSystem.Cartesian), new Coordinate(LRF_MAX_RELIABLE_DISTANCE, 0, CoordSystem.Cartesian));
 
+            // Check if leftmost Coordinate in the leftmost Region is on the right half of the entire FOV. If it is, make leftEdgeCoordinate where the 
+            // max -acceptable-range meets the left FOV line, since the FindClosestPointOnLine function return will cause errors for 180 degree FOV.
+            Coordinate leftEdgeCoordinate = Line.FindClosestPointOnLine(lrfLeftFOV, firstRegion.StartCoordinate);
+            if (firstRegion.StartCoordinate.X > 0)
+            {
+                leftEdgeCoordinate = new Coordinate(-AI_ROCKS.Services.AutonomousService.OBSTACLE_DETECTION_DISTANCE, 0, CoordSystem.Cartesian);
+            }
+            Line leftEdgeGap = new Line(leftEdgeCoordinate, firstRegion.StartCoordinate);
+            // give leftEdgeGap the old checkerooski!
+            if (leftEdgeGap.Length >= threshold)
+            {
+                Coordinate leftMidPoint = leftEdgeGap.FindMidpoint();
+                if (leftMidPoint.X > 0)
+                {
+                    angle = currCompass + (90 - (Math.Atan2(leftMidPoint.Y, leftMidPoint.X) * (180 / Math.PI)));
+                    angle = angle % 360;
+                }
+                else if (leftMidPoint.X < 0)
+                {
+                    angle = currCompass - (90 - (Math.Atan2(-1 * leftMidPoint.Y, leftMidPoint.X) * (180 / Math.PI)));
+                    angle = angle % 360;
+                }
+                else // midpoint.X is 0
+                {
+                    angle = currCompass; // so elegant :)
+                }
+                if (Math.Abs(idealDirection - angle) < bestAngle)
+                {
+                    bestAngle = angle;
+                    bestGap = leftEdgeGap;
+                }
+            }
+
+            // Check if rightmost Coordinate in the rightmost Region is on the left half of the entire FOV. If it is, make rightEdgeCoordinate where the
+            // max -acceptable-range meets the right FOV line, since the FindClosestPointOnLine function return will cause errors for 180 degree FOV.
+            Coordinate rightEdgeCoordinate = Line.FindClosestPointOnLine(lrfRightFOV, lastRegion.EndCoordinate);
+            if (lastRegion.EndCoordinate.X < 0)
+            {
+                rightEdgeCoordinate = new Coordinate(AI_ROCKS.Services.AutonomousService.OBSTACLE_DETECTION_DISTANCE, 0, CoordSystem.Cartesian);
+            }
+            Line rightEdgeGap = new Line(rightEdgeCoordinate, lastRegion.EndCoordinate);
+            // give rightEdgeGap the old checkerooski!
+            if (rightEdgeGap.Length >= threshold)
+            {
+                Coordinate rightMidPoint = rightEdgeGap.FindMidpoint();
+                if (rightMidPoint.X > 0)
+                {
+                    angle = currCompass + (90 - (Math.Atan2(rightMidPoint.Y, rightMidPoint.X) * (180 / Math.PI)));
+                    angle = angle % 360;
+                }
+                else if (rightMidPoint.X < 0)
+                {
+                    angle = currCompass - (90 - (Math.Atan2(-1 * rightMidPoint.Y, rightMidPoint.X) * (180 / Math.PI)));
+                    angle = angle % 360;
+                }
+                else // midpoint.X is 0
+                {
+                    angle = currCompass; // so elegant :)
+                }
+                if (Math.Abs(idealDirection - angle) < bestAngle)
+                {
+                    bestAngle = angle;
+                    bestGap = rightEdgeGap;
+                }
+            }
+            
             //start to iterate through the list to find the bestGap
             for (int i = 0; i < regions.Count - 2; i++) // don't iterate through entire list, will result in index out of bounds error on rightRegion assignment
             {
@@ -164,11 +244,8 @@ namespace AI_ROCKS.Drive.DriveStates
                 }
             }
             return bestGap;
+            // may need to add code in taking into account slope! 
         }
-
-
-
-
-
+        
     }
 }
