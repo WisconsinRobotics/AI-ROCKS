1f1433afd880bf15a177401312cf302a2af870cc
jbenthein <jbenthein@gmail.com>
2017-03-07 20:04:28 -0600

    initial commit for VisionDriveState

--- AI-ROCKS/Drive/DriveStates/VisionDriveState.cs
+++ AI-ROCKS/Drive/DriveStates/VisionDriveState.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 
 using ObstacleLibrarySharp;
+using AForge.Video;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
 
 namespace AI_ROCKS.Drive.DriveStates
 {
+
     class VisionDriveState : IDriveState
     {
+        MJPEGStream stream;
+        const string camera_url = "http://192.168.1.8/cgi-bin/mjpg/video.cgi";
+        const string camera_login = "admin";
+        const string camera_password = "i#3Er0b0";
+
+        // Constants for hough circles
+        const int param1 = 12;
+        const int param2 = 24;
+        const double dp = 1.5;
+        const double minDist = 100;
+        const int minRadius = 1;
+
+        // Limits of HSV masking
+        Hsv lower = new Hsv(22, 120, 114);
+        Hsv upper = new Hsv(37, 255, 255);
+
+        const int leftThreshold = 200;
+        const int rightThreshold = 300;
+
+        float X;
+        DriveCommand command;
+
         public VisionDriveState()
-        {    
+        {                
+            stream = new MJPEGStream(camera_url);
+            stream.Login = camera_login;
+            stream.Password = camera_password;
+            stream.ForceBasicAuthentication = true;
+            stream.NewFrame += NetworkCamGrab;
+            stream.Start();
+            X = 0;
         }
 
         /// <summary>
@@ -16,7 +50,26 @@ namespace AI_ROCKS.Drive.DriveStates
         /// <returns>DriveCommand - the next drive command for ROCKS to execute.</returns>
         public DriveCommand FindNextDriveCommand()
         {
-            return null;
+            if (X < 0)
+            {
+                // Ball not found
+                return command = new DriveCommand(0, 0, 0);
+            }
+            else if (X < leftThreshold) 
+            {
+                // Ball is to the left of us
+                return command = new DriveCommand(-1, 1, 1);
+            }
+            else if (X > rightThreshold)
+            {
+                // Ball is to the right of us
+                return command = new DriveCommand(1, -1, 1);
+            }
+            else 
+            {
+                // Ball is straight ahead
+                return command = new DriveCommand(0, 0, 1);
+            }
         }
 
         /// <summary>
@@ -39,7 +92,28 @@ namespace AI_ROCKS.Drive.DriveStates
 
             // TODO Vision group's algorithm here
 
+
             return null;
+        }
+
+        private void NetworkCamGrab(Object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap b = eventArgs.Frame;
+            Image<Bgr, byte> myImage = new Image<Bgr, byte>(b);
+            ProcessFrame(myImage);
+        }
+
+        private void ProcessFrame(Image<Bgr, byte> m)
+        {
+            Image<Bgr, byte> blur = m.SmoothGaussian(15);
+            Image<Hsv, byte> hsv = blur.Convert<Hsv, byte>();
+            Image<Gray, byte> mask = hsv.InRange(lower, upper);
+            Image<Bgr, byte> coloredMask = mask.Convert<Bgr, byte>() & m;
+            Image<Gray, byte> grayMask = coloredMask.Convert<Gray, byte>();
+
+            CircleF[] c = grayMask.Convert<Gray, byte>().HoughCircles(new Gray(param1), new Gray(param2), dp, minDist, minRadius)[0];
+            if (c.Length > 0)
+                X = c[0].Center.X;
         }
     }
 }
